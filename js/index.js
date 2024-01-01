3d6c7e8299dbf4bf69d7d3ec00a699a3e89440c1
odeisir <odeisiruela@alumnos.cei.es>
2024-01-01 21:19:27 +0100

    efecto hover hero__window js wip

--- js/index.js
+++ js/index.js
@@ -1,67 +1,6 @@
 'use strict'
-
-
-// Cuando hago click en #readme
-// le remove la clase selected a #exe
-// le add la clase selected a #readme
-
-// Cuando hago click en #hero
-// le remove la clase selected #readme
-// le add la clase selected a #exe
-
-// Si la clase selected == a false
-// entonces añadimos style.blur
-
-// Seleccionamos #readme y #exe
-
-
-// Cuando hago click en #readme...
-// readme.addEventListener('click', function(){
-//     exe.classList.remove('selected')
-//     readme.classList.add('selected')
-//     console.log(readme.classList)
 // })
-
-// //Cuando hago click en #exe...
-// exe.addEventListener('click', function(){
-//     readme.classList.remove('selected')
-//     exe.classList.add('selected')
-//     console.log(readme.classList)
-// })
-
-
-// Quiero que cuando hago click en las ventanas, se ponga por delante la ventana clicada
-// La ventana readme.exe ha de estar por delante por defecto
-
 // Determino el valor por defecto de translateZ (2rem)
-// Determino el valor de translateZ del elemento delantero (+1rem)
-// Determino el valor de translateZ del elemento trasero (-1rem)
-// Determino el valor por defecto de translateZ en #readme (3rem)
-// Determino el valor por defecto de translateZ en #exe (2rem)
-// Aplico los valores por defecto a exe y readme
-// Cuando hago click en #readme 
-// Aplico el valor translateZ del elemento delantero a readme
-// Aplico el valor translateZ del elemento trasero exe
-// Cuando hago click en #exe
-// Aplico el valor translateZ del elemento delantero exe
-// Aplico el valor translateZ del elemento traseto readme
-
-let defaultZ = 2
-let readmeDefaultZ = defaultZ + 1 
-const sendFront = defaultZ + 1
-const sendBack = defaultZ - 1
-const readme = document.querySelector('#readme')
-const exe = document.querySelector('#exe')
-const falseWindow = document.querySelectorAll('.hero__window')
-let windowRotateDefault = -20
-
-falseWindow.forEach( function (eachFalseWindow , i){
-    falseWindow[i].style.transform = `translateZ(${defaultZ}) rotateY(${windowRotateDefault}deg)`
-})
-
-console.log(falseWindow)
-
-
 // Cuando hago click en #close dentro de readme
 // ADD la clase hidden a readme
 
@@ -78,3 +17,46 @@ console.log(falseWindow)
 //     exe.classList.add('hidden')
 // })
 
+// Quiero crear varias interacciones con hero__window:
+//  - Animación en hover
+//  - Cerrar ventana cuando click en close
+//  - Minimizar ventana cuando click en min
+//  - Cambiar translateZ cuando click en cada ventana para poner una encima de la otra
+// - Drag and drop de las ventanas
+//Transformación default de las ventanas
+// Seleccionamos TODAS las .hero__window
+// Definimos los valores por defecto de translatez, rotateY, SkewY y position (bottom y left)
+// Aplicamos los estilos a TODAS las .hero__window
+    const heroWindows = document.querySelectorAll('.hero__window')
+    
+    const winDefTranslZ = 5
+    const winDefRotY = 30
+    const winDefSkewY = -3
+    const winDefBot = 10
+    const winDefLeft = 5
+
+   heroWindows.forEach(function (eachWindow){
+    heroWindows.style.transform = `translateZ(${winDefTranslZ}rem) rotateY(${winDefRotY}deg) skewY(${winDefSkewY}deg)`
+    heroWindows.style.bottom = `${winDefBot}%`
+    heroWindows.style.left = `${winDefLeft}%`
+   })
+
+    //Hover de las ventanas "efecto 3D"
+    // Definimos los valores de translatez, rotatey y skewy en hover
+    //Cuando mouseover CUALQUIER hero__window aplica la transformación hover
+    // cambiamos los valores default por hover
+    // cuando mouseout
+    // volvemos a poner los valores por defecto
+    const hoverTranslZ = 0
+    const hoverRotY = 0
+    const hoverSkewY = 0
+
+heroWindows.forEach(function(eachWindow){
+    heroWindows.addEventListener('mouseover' , function(){
+        heroWindows.style.transform = `translateZ(${hoverTranslZ}rem) rotateY(${hoverRotY}deg) skewY(${hoverSkewY}deg)`
+    })
+
+    heroWindows.addEventListener('mouseout' , function(){
+        heroWindows.style.transform = `translateZ(${winDefTranslZ}rem) rotateY(${winDefRotY}deg) skewY(${winDefSkewY}deg)`
+    })
+})
