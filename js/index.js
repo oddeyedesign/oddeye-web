c877ad39c1c1a636aaf42d7b49eddec249be3b55
odeisir <odeisiruela@alumnos.cei.es>
2024-01-02 23:50:28 +0100

    js hero__window
    // #readme: cuando hago click se le add clase front, se le remove back y le add back/remove front a exe
    // #exe: cuando hago click se le add la clase front, se le remove back y e add back/remove front a readme
    // If front: aplicamos la clase hover en mouseover y se la remove en mouseout

--- js/index.js
+++ js/index.js
@@ -7,12 +7,12 @@
 //cuando hago click en #close button dentro de exe
 // ADD la clase hidden a exe
 
-// const closeReadMe = document.querySelector('#readme .buttons__close')
+// const closeReadMe = document.querySelector('#readme .button__close')
 // closeReadMe.addEventListener('click' , function(){
 //     readme.classList.add('hidden')
 // })
 
-// const closeExe = document.querySelector('#exe .buttons__close')
+// const closeExe = document.querySelector('#exe .button__close')
 // closeExe.addEventListener('click' , function(){
 //     exe.classList.add('hidden')
 // })
@@ -27,36 +27,138 @@
 // Seleccionamos TODAS las .hero__window
 // Definimos los valores por defecto de translatez, rotateY, SkewY y position (bottom y left)
 // Aplicamos los estilos a TODAS las .hero__window
-    const heroWindows = document.querySelectorAll('.hero__window')
+//     const heroWindows = document.querySelectorAll('.hero__window')
+//     const readme = document.querySelector('#readme')
+//     const exe = document.querySelector('#exe')
+
     
-    const winDefTranslZ = 5
-    const winDefRotY = 30
-    const winDefSkewY = -3
-    const winDefBot = 10
-    const winDefLeft = 5
+//     const front = 10
+//     const back = 0
+//     const winDefRotY = 30
+//     const winDefSkewY = -3
+//     const winDefBot = 10
+//     const winDefLeft = 5
 
-   heroWindows.forEach(function (eachWindow){
-    heroWindows.style.transform = `translateZ(${winDefTranslZ}rem) rotateY(${winDefRotY}deg) skewY(${winDefSkewY}deg)`
-    heroWindows.style.bottom = `${winDefBot}%`
-    heroWindows.style.left = `${winDefLeft}%`
-   })
+//    heroWindows.forEach(function (eachWindow , i){
+//     heroWindows[i].style.transform = `translateZ(${front}rem) rotateY(${winDefRotY}deg) skewY(${winDefSkewY}deg)`
+//     heroWindows[i].style.bottom = `${winDefBot}%`
+//     heroWindows[i].style.left = `${winDefLeft}%`
+//     exe.style.transform=`translateZ(${back}rem)`
+//    })
 
-    //Hover de las ventanas "efecto 3D"
-    // Definimos los valores de translatez, rotatey y skewy en hover
-    //Cuando mouseover CUALQUIER hero__window aplica la transformación hover
-    // cambiamos los valores default por hover
-    // cuando mouseout
-    // volvemos a poner los valores por defecto
-    const hoverTranslZ = 0
-    const hoverRotY = 0
-    const hoverSkewY = 0
+//     //Hover de las ventanas "efecto 3D"
+//     // Definimos los valores de translatez, rotatey y skewy en hover
+//     //Cuando mouseover CUALQUIER hero__window aplica la transformación hover
+//     // cambiamos los valores default por hover
+//     // cuando mouseout
+//     // volvemos a poner los valores por defecto
+//     const hoverRotY = 0
+//     const hoverSkewY = 0
 
-heroWindows.forEach(function(eachWindow){
-    heroWindows.addEventListener('mouseover' , function(){
-        heroWindows.style.transform = `translateZ(${hoverTranslZ}rem) rotateY(${hoverRotY}deg) skewY(${hoverSkewY}deg)`
-    })
+// heroWindows.forEach(function(eachWindow , i){
+//     heroWindows[i].addEventListener('mouseover' , function(){
+//         heroWindows[i].style.transform = `translateZ(${front}rem) rotateY(${hoverRotY}deg) skewY(${hoverSkewY}deg)`
+//     })
 
-    heroWindows.addEventListener('mouseout' , function(){
-        heroWindows.style.transform = `translateZ(${winDefTranslZ}rem) rotateY(${winDefRotY}deg) skewY(${winDefSkewY}deg)`
-    })
+//     heroWindows[i].addEventListener('mouseout' , function(){
+//         heroWindows[i].style.transform = `translateZ(${front}rem) rotateY(${winDefRotY}deg) skewY(${winDefSkewY}deg)`
+//     })
+// })
+
+// #readme: cuando hago click se le add clase front, se le remove back y le add back/remove front a exe
+// #exe: cuando hago click se le add la clase front, se le remove back y e add back/remove front a readme
+// If front: aplicamos la clase hover en mouseover y se la remove en mouseout
+// .hero__window: cuando hago click en el bottón close le aplico clase hidden a hero__window
+// .window__content: Cuando hago click en min aplico clase hidden a window__content y a min y se la quito a res
+// res: cuando hago click se le add la classe hidden de nuevo y se la remove a min
+
+// Selecciono los elementos
+const readme = document.getElementById('readme')
+const exe = document.getElementById('exe')
+const heroWin = document.querySelectorAll('.hero__window')
+const clo = document.querySelectorAll('.button__close')
+const min = document.querySelectorAll('.button__min')
+const res = document.querySelectorAll('.button__res')
+const winContent = document.querySelectorAll('.window__content')
+
+console.log(readme)
+console.log(exe)
+console.log(heroWin)
+console.log(clo)
+console.log(min)
+console.log(res)
+
+// Cuando hago CLICK en #readme
+// REMOVE la clase front a #exe
+// ADD la clase back a #exe
+// REMOVE la clase back a #readme
+// ADD la clase front a #readme
+readme.addEventListener('click' , function(){
+    exe.classList.remove('front')
+    exe.classList.add('back')
+    readme.classList.remove('back')
+    readme. classList.add('front')
 })
+
+// Cuando hago MOUSEOVER en #readme
+// SI #readme contiene la clase front
+// ADD la clase hover a #readme
+readme.addEventListener('pointerover' , function(){
+    if (readme.classList.contains('front')){
+    console.log('readme is front')
+    readme.classList.add('hover')
+}})
+
+// Cuando hago MOUSEOUT en #readme
+// SI readme coniene la clase front
+// REMOVE la clase hover a #readme
+readme.addEventListener('pointerout' , function(){
+    if(readme.classList.contains('front')){
+       readme.classList.remove('hover')
+    }
+})
+
+// Cuando hago CLICK en #exe
+// REMOVE la clase front a #readme
+// ADD la clase back a #readme
+// REMOVE la clase back a #exe
+// ADD la clase front a #exe
+exe.addEventListener('click' , function(){
+    readme.classList.remove('front')
+    readme. classList.add('back')
+    exe.classList.remove('back')
+    exe.classList.add('front')
+})
+
+
+// Cuando hago MOUSEOVER en #exe
+// SI #exe contiene la clase front
+// ADD la clase hover a #exe
+exe.addEventListener('mouseover' , function(){
+    if(exe.classList.contains('front')){
+        console.log('exe is front')
+        exe.classList.add('hover')
+    }
+})
+
+// Cuando hago MOUSEOUT en #exe
+// SI exe coniene la clase front
+// REMOVE la clase hover a #exe
+exe.addEventListener('mouseout' , function(){
+    if(exe.classList.contains('front')){
+        exe.classList.remove('hover')
+    }
+})
+
+//Cuando hago click en CUALQUIER .button__close
+// se le ADD la clase hidden a .hero__window
+
+// Cuando hago click en CUALQUIER .button__min
+// se le add la clase hidden a .button__min mismo index
+// se le add la clase hidden a .window__content mismo index
+// se le remove la clase hidden a .button__res mismo index
+
+//Cuando hago click en CUALQUIER .button___res
+// se le remove la clase hidden a .button__min mismo index
+//se le add la clase hidden a .button__res
+
