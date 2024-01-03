9fd04b563e632a1214990d993009f0e902664a72
odeisir <odeisiruela@alumnos.cei.es>
2024-01-03 23:57:12 +0100

    js interacción logo (no funciona)

--- js/index.js
+++ js/index.js
@@ -1,69 +1,4 @@
 'use strict'
-// })
-// Determino el valor por defecto de translateZ (2rem)
-// Cuando hago click en #close dentro de readme
-// ADD la clase hidden a readme
-
-//cuando hago click en #close button dentro de exe
-// ADD la clase hidden a exe
-
-// const closeReadMe = document.querySelector('#readme .button__close')
-// closeReadMe.addEventListener('click' , function(){
-//     readme.classList.add('hidden')
-// })
-
-// const closeExe = document.querySelector('#exe .button__close')
-// closeExe.addEventListener('click' , function(){
-//     exe.classList.add('hidden')
-// })
-
-// Quiero crear varias interacciones con hero__window:
-//  - Animación en hover
-//  - Cerrar ventana cuando click en close
-//  - Minimizar ventana cuando click en min
-//  - Cambiar translateZ cuando click en cada ventana para poner una encima de la otra
-// - Drag and drop de las ventanas
-//Transformación default de las ventanas
-// Seleccionamos TODAS las .hero__window
-// Definimos los valores por defecto de translatez, rotateY, SkewY y position (bottom y left)
-// Aplicamos los estilos a TODAS las .hero__window
-//     const heroWindows = document.querySelectorAll('.hero__window')
-//     const readme = document.querySelector('#readme')
-//     const exe = document.querySelector('#exe')
-
-
-//     const front = 10
-//     const back = 0
-//     const winDefRotY = 30
-//     const winDefSkewY = -3
-//     const winDefBot = 10
-//     const winDefLeft = 5
-
-//    heroWindows.forEach(function (eachWindow , i){
-//     heroWindows[i].style.transform = `translateZ(${front}rem) rotateY(${winDefRotY}deg) skewY(${winDefSkewY}deg)`
-//     heroWindows[i].style.bottom = `${winDefBot}%`
-//     heroWindows[i].style.left = `${winDefLeft}%`
-//     exe.style.transform=`translateZ(${back}rem)`
-//    })
-
-//     //Hover de las ventanas "efecto 3D"
-//     // Definimos los valores de translatez, rotatey y skewy en hover
-//     //Cuando mouseover CUALQUIER hero__window aplica la transformación hover
-//     // cambiamos los valores default por hover
-//     // cuando mouseout
-//     // volvemos a poner los valores por defecto
-//     const hoverRotY = 0
-//     const hoverSkewY = 0
-
-// heroWindows.forEach(function(eachWindow , i){
-//     heroWindows[i].addEventListener('mouseover' , function(){
-//         heroWindows[i].style.transform = `translateZ(${front}rem) rotateY(${hoverRotY}deg) skewY(${hoverSkewY}deg)`
-//     })
-
-//     heroWindows[i].addEventListener('mouseout' , function(){
-//         heroWindows[i].style.transform = `translateZ(${front}rem) rotateY(${winDefRotY}deg) skewY(${winDefSkewY}deg)`
-//     })
-// })
 
 // #readme: cuando hago click se le add clase front, se le remove back y le add back/remove front a exe
 // #exe: cuando hago click se le add la clase front, se le remove back y e add back/remove front a readme
@@ -197,7 +132,9 @@ resBtn.forEach(function (eachRes, i) {
 // Y readme contiene la clase hidden
 // Remove back de exe
 // Add front a exe
+
 // No funciona, revisar si da tiempo
+
 // if(exe.classList.contains('back') || readme.classList.contains('hidden')){
 //     exe.classList.remove('back')
 //     exe.classList.add('front')
@@ -212,4 +149,95 @@ resBtn.forEach(function (eachRes, i) {
 //     readme.classList.remove('back')
 //     readme.classList.add('front')
 // }
+
+// Interacción del Logo: ojos que siguen al cursor
+// Sigo este tutorial: https://www.youtube.com/watch?v=cwT0oLU8iF4
+// El tutorial no está comentando y no entiendo bien que hace
+// porque es un código muy avanzado asi que intento desgranarlo
+// para entenderlo lo mejor posible y no utilizar el código al tuntún
+
+//Selecciono en el documento las partes del SVG a las que quiero
+// aplicar la interacción:
+// #face
+// #snout
+// #firstPupil
+// #seconPupil
+// #thirdPupil
+
+const face = document.getElementById('face')
+const snout = document.getElementById('snout')
+const pupil = document.querySelectorAll('.eye__pupil')
+
+console.log(pupil)
+
+// Definimos la posición del cursor en 0 en los ejes xy
+// Definimos el width del viewport en px con innerWidth
+// https://developer.mozilla.org/en-US/docs/Web/API/Window/innerWidth
+// Definimos el height del viewport en pixeles con innerHeight
+// https://developer.mozilla.org/es/docs/Web/API/Window/innerHeight
+// Uso let porque los valores van a cambiar
+let cursorPosition = {x:0 , y:0}
+let viewportWidth = window.innerWidth
+let windowHeight = window.innerHeight
+
+// Definimos el tamaño de la ventana con una función
+// La función contiene viewportHeight y viewportWidth
+function defineScreenSize(){
+    viewportHeight = window.innerHeight
+    viewportWidth = window.innerWidth
+}
+
+// Definimos la posicion actual del cursor en la variable cursorPosition
+// X = e.clientX
+// Y = e.clientY
+// https://www.w3schools.com/jsref/event_clientx.asp
+// Llamamos a la función follow para seguir el movimiento del mouse
+// *Follow se define más adelante
+function pointerMove(e){
+    cursorPosition = {x:e.clientX, y:e.clientY}
+    follow()
+}
+// Definimos lookAtCursor con una función
+// Sus parámetros son element, xRatio e yRatio
+// Element = elementos a trasnformar
+// xRatio = distancia entre element y el cursor en eje X
+// yRatio = distancia entre el element y el cursor en eje Y
+
+// La posición de element la define get.boundingClientRect
+// Que devuelve el tamaño y la posición del elemento en relación al viewport
+//https://developer.mozilla.org/en-US/docs/Web/API/Element/getBoundingClientRect
+
+// Calculamos el centro del elemento en el eje X: Ancho del elemento en x + ancho del elemento/2
+// Calculamos el centro del elemento en el eje Y: Alto del elemento en y + alto del elemento/2
+
+// Calculamos la distancia entre el cursor y el centro del elemento en ambos ejes
+// Redondondeamos: cursorPosition.x - menos centerX * 100 / window.innerWidth
+// Redondeamos: cursorPosition.y - menos centerY * 100 / window.innerHeight
+// Esto nos da el vamor redondeado del porcentaje del alto y ancho de la ventana
+
+// Aplicamos la transformación al estilo del elemento con translate calculando dos valores:
+// la distancia entre el cursor y distanceLeft/xRatio 
+// la distancia entre el cursor y distanceTop/yRatio
+
+const lookAtCursor = (element , xRatio, yRatio) =>{
+    const elementOffset = element.getBoundingClientRect()
+    const centerX = elementOffset.x + elementOffset.width / 2
+    const centerY = elementOffset.y + elementOffset.height / 2
+    const distanceLeft = Math.round(((cursorPosition.x - centerX)*100)/window.innerWidth)
+    const distanceTop = Math.round(((cursorPosition.y - centerY)*100)/window.innerHeight)
+    element.style.transform = `translate(${distanceLeft/xRatio}rem, ${distanceTop/yRatio}rem)`
+}
+
+// Definimos la posición actual del elemento con la follow (que está en la función mousemove)
+// SI el elemento es X lookAtCursor (elemento, valor de xRatio, valor de yRatio)
+const follow = function(){
+    if(pupil) lookAtCursor(pupil, 2.8, 2.8)
+    if(snout) lookAtCursor(snout, 3.2, 3.2)
+    if(face) lookAtCursor(face, 2.8, 2.8)
+}
+
+// Cuando hago resize en la ventana, activo la función defineScreenSize
+// Cuando hago mousemove en la ventana, activo la función mouseMove
+window.addEventListener('resize' , defineScreenSize)
+window.addEventListener('pointermove', pointerMove)
 
